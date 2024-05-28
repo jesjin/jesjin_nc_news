e6118d2a9280a03583df1d5931838a616d9554aa
jesjin <jess.hk.jin@gmail.com>
2024-05-28 21:09:11 +0100

    completes task3 03-GET-api

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const { getTopics } = require('./controllers/topicController');
+const { getApiEndPoints } = require('./controllers/apiController');
 
 const app = express();
 
 app.get('/api/topics', getTopics);
+app.get('/api', getApiEndPoints);
+
 app.use((err, req, res, next) => {
   if (err.code === '22P02') {
     res.status(400).send({ msg: 'Bad Request' });
