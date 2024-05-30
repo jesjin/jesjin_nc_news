b2450fa2967e8a4aaec89e7b08bc823a14dbc36a
jesjin <jess.hk.jin@gmail.com>
2024-05-30 20:04:01 +0100

    Complete GET /api/users

--- app.js
+++ app.js
@@ -1,35 +1,38 @@
-const express = require('express');
-const { getTopics } = require('./controllers/topicController');
-const { getApiEndPoints } = require('./controllers/apiController');
-const { 
-  getArticleById, 
-  getAllArticles, 
-  getCommentsByArticleId, 
-  postCommentByArticleId, 
+const express = require("express");
+const { getTopics } = require("./controllers/topicController");
+const { getApiEndPoints } = require("./controllers/apiController");
+const {
+  getArticleById,
+  getAllArticles,
+  getCommentsByArticleId,
+  postCommentByArticleId,
   patchArticleVotes,
   deleteCommentById,
-} = require('./controllers/articleController');
+} = require("./controllers/articleController");
+const { getAllUsers } = require("./controllers/userController");
 
 const app = express();
 
 app.use(express.json());
 
-app.delete('/api/comments/:comment_id', deleteCommentById);
-app.patch('/api/articles/:article_id', patchArticleVotes);
-app.post('/api/articles/:article_id/comments', postCommentByArticleId);
-app.get('/api/articles/:article_id/comments', getCommentsByArticleId);
-app.get('/api/articles', getAllArticles);
-app.get('/api/articles/:article_id', getArticleById);
-app.get('/api/topics', getTopics);
-app.get('/api', getApiEndPoints);
+app.get("/api/users", getAllUsers);
+app.delete("/api/comments/:comment_id", deleteCommentById);
+app.patch("/api/articles/:article_id", patchArticleVotes);
+app.post("/api/articles/:article_id/comments", postCommentByArticleId);
+app.get("/api/articles/:article_id/comments", getCommentsByArticleId);
+app.get("/api/articles", getAllArticles);
+app.get("/api/articles/:article_id", getArticleById);
+app.get("/api/topics", getTopics);
+app.get("/api", getApiEndPoints);
 
 app.use((err, req, res, next) => {
-  if (err.code === '22P02') {
-    res.status(400).send({ msg: 'Bad Request' });
+  if (err.code === "22P02") {
+    res.status(400).send({ msg: "Bad Request" });
   } else if (err.status) {
     res.status(err.status).send({ msg: err.msg });
   } else {
-    res.status(500).send({ msg: 'Internal Server Error' });
+    console.log(err);
+    res.status(500).send({ msg: "Internal Server Error" });
   }
 });
 
